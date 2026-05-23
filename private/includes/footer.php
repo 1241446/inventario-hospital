0d0d76b70cdf31af83a0661d657d714e72186673
Carlota Santos <1241446@isep.ipp.pt>
2026-05-23 15:27:58 +0100

    Criar rodape PHP reutilizavel

---
+++ private/includes/footer.php
@@ -0,0 +1,13 @@
+    </div><!-- /main -->
+</div><!-- /layout -->
+
+<!-- JavaScript partilhado -->
+<script src="<?= APP_BASE ?>/assets/js/1241446.js"></script>
+
+<?php if (!empty($scriptsExtra)): ?>
+    <!-- Scripts específicos da página -->
+    <?= $scriptsExtra ?>
+<?php endif; ?>
+
+</body>
+</html>
